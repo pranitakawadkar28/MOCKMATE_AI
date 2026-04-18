33bca6aa168a7ebd10b47879a1ac223614697080
Pranita Kawadkar <pranitakawadkar@Pranitas-MacBook-Air.local>
2026-04-18 22:12:04 +0530

    implement axios and redux toolkit setUp

---
+++ Client/src/features/auth/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+  loading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    authStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    authSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    authFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { authStart, authSuccess, authFailure, logout } = authSlice.actions;
+
+export default authSlice.reducer;
