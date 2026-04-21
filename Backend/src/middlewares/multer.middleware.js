ef3294b85274d52dda619f9fad8b5821706a4844
Pranita Kawadkar <pranitakawadkar@Pranitas-MacBook-Air.local>
2026-04-21 18:33:20 +0530

    refactor: enhance multer middleware
    - add PDF-only file filter
    - integrate AppError for invalid uploads
    - keep 5MB file size limit
    - improve upload validation

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,16 +1,28 @@
 import multer from "multer";
+import path from "path";
+import { AppError } from "../utils/AppError.js";
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "public")
-    },
-    filename: function(req, file, cb){
-        const filename = Date.now() + "-" + file.originalname;
-        cb(null, filename)
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "public");
+  },
+  filename: function (req, file, cb) {
+    const filename = Date.now() + "-" + file.originalname;
+    cb(null, filename);
+  },
+});
 
-export const upload = multer ({
-    storage,
-    limits: { fileSize: 5 * 1024 * 1024} //5MB limit
+const fileFilter = (req, file, cb) => {
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (ext === ".pdf" && file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new AppError("ONLY_PDF_FILES_ALLOWED", 400), false);
+  }
+};
+
+export const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, //5MB limit
+  fileFilter,
 });
