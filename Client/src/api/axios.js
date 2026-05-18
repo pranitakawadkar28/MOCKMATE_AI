09448d677e350fef70bf3773b4821ebc160cc71d
Pranita Kawadkar <pranitakawadkar@Pranitas-MacBook-Air.local>
2026-05-18 12:13:45 +0530

    axios backend utl

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8001/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
